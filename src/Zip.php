b5bfce381c12fb86d4a335c52892f7e4d9cb253e
asahi <waithawoocw@gmail.com>
2021-12-20 02:32:51 +0630

    phpzipper : first commit

---
+++ src/Zip.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace WaiThaw\PhpZipper;
+
+use \ZipArchive;
+use \Exception;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+
+class Zip
+{
+    private $zip;
+
+
+    private $zipfile;
+
+    public function __construct()
+    {
+    }
+
+    public function createFromFiles($zipfile, $password = null, $files,  $flag = ZipArchive::CREATE)
+    {
+        $zip = self::openZip($zipfile, $flag);
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                // $zip->addFile($file, substr($file,strrpos($file,'/')+1));
+                $zip->addFile($file, basename($file));
+                if($password != null){
+                    $zip->setEncryptionName(basename($file), ZipArchive::EM_AES_256, $password);
+                }
+            }
+        }
+        
+        $zip->close();
+        $this->zipfile = $zipfile;
+
+        return $this;
+    }
+
+    public function createFromDir( $zipfile, $directory, $password = null, $flag = ZipArchive::CREATE)
+    {   
+        $pathInfo = pathinfo($directory);
+            $parentPath = $pathInfo['dirname'];
+            $dirName = $pathInfo['basename'];
+            $zip = self::openZip($zipfile, $flag);
+
+            self::dir2zip($zip, $directory, $password);
+            $zip->close();
+
+            $this->zipfile = $zipfile;
+
+            return $this;
+    }
+
+    private static function dir2zip($zip, $directory, $password = null)
+    {
+
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory), RecursiveIteratorIterator::LEAVES_ONLY);
+        foreach ($files as $name => $file)
+        {
+            $filePath = $file->getRealPath();
+            var_dump($filePath);
+            $relativePath = substr($filePath, strlen($directory)+1);
+
+            if (!$file->isDir())
+            {
+                $zip->addFile($filePath, $relativePath);
+                if($password != null){
+                    $zip->setEncryptionName($relativePath, ZipArchive::EM_AES_256, $password);
+                }
+            }else {
+                if($relativePath !== false){
+                    $zip->addEmptyDir($relativePath);
+                    if($password != null){
+                        $zip->setEncryptionName($relativePath, ZipArchive::EM_AES_256, $password);
+                    }
+                }
+            }
+        }
+
+    }
+    private static function openZip($zipfile, $flags = null)
+    {
+        $zip = new ZipArchive();
+        $open_zip = $zip->open($zipfile, $flags);
+        if ($open_zip !== true) {
+            throw new \Exception("OPEN_ERROR");
+        }
+        return $zip;
+    }
+
+    public function download($unlink = null)
+    {
+        header('Content-type: application/zip');
+        header('Content-Disposition: attachment; filename="'.basename($this->zipfile).'"');
+        header("Content-length: " . filesize($this->zipfile));
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        ob_clean();
+        flush();
+        readfile($this->zipfile);
+        if($unlink == 'delete'){
+            unlink($this->zipfile);
+        }
+        exit;
+
+    }
+
+
+  
+}
